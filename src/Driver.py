e7b07b33ad9eb67de52d6801abf9bf4a6aadf057
SeenivasanSeeni <seeni0424@gmail.com>
2018-08-16 14:52:28 +0530

    training done

--- src/Driver.py
+++ src/Driver.py
@@ -2,14 +2,19 @@ import Dataset
 import Model
 
 def train(dat,Mod):
-    iterations=20
+    iterations=35
 
     for i in range(iterations):
         images,labels=dat.getBatch()
         lo,acc=Mod.train(images,labels)
         print("Iteration:",i,"Loss",lo,"Acc",acc)
-
+        import pdb
+        pdb.set_trace()
     return
+
+def test(dat,Mod):
+    pass
+
 
 def main():
     dat=Dataset.Dataset()
